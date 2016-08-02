21c08f089c433272c6a2e0962fc0464d3937dc9b
DmitryZlotin <dmitryzlotin@gmail.com>
2016-08-02 20:24:34 +0300

    Test

---
+++ Scripts/CTRL.js
@@ -0,0 +1,16 @@
+﻿var purchaseApp = angular.module("purchaseApp", []);
+purchaseApp.controller("purchaseController", function ($scope) {
+    $scope.list = model;
+    $scope.sum = 0;
+    for (item in $scope.list.items) {
+        $scope.sum = $scope.sum + $scope.list.items[item].price;//считаем сумму всего списка
+    }
+    $scope.addItem = function (text, price) {
+        price = parseFloat(price); // преобразуем введенное значение к числу
+        if (text != "" && !isNaN(price)) // если текст установлен и введено число, то добавляем
+        {
+            $scope.list.items.push({ purchase: text, price: price, done: false });
+            $scope.sum += price;
+        }
+    }
+});
---
+++ Scripts/Model.js
@@ -0,0 +1,8 @@
+﻿var model = {
+    items: [
+        { purchase: "לחם", done: false, price: 15.9 },
+        { purchase: "שמן", done: false, price: 60 },
+        { purchase: "תפוחי אדמה", done: true, price: 22.6 },
+        { purchase: "גבינה", done: false, price: 310 }
+    ]
+};
